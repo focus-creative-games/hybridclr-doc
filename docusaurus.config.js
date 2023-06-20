206ab2099108496c1ac3b4419293ecb1b7464bd0
walon <walon@focus-creative-games.com>
2023-06-20 23:28:53 +0800

    支持搜索

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -161,6 +161,35 @@ const config = {
         darkTheme: darkCodeTheme,
         additionalLanguages: ['csharp', 'cpp'],
       },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'MHX0T95QPO',
+  
+        // Public API key: it is safe to commit it
+        apiKey: '6f676c54be649ee5890b35572a9e7ba3',
+  
+        indexName: 'hybridclrcode-philosophy',
+  
+        // Optional: see doc section below
+        contextualSearch: true,
+  
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: 'external\\.com|domain\\.com',
+  
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        // replaceSearchResultPathname: {
+        //   from: '/docs/', // or as RegExp: /\/docs\//
+        //   to: '/',
+        // },
+  
+        // Optional: Algolia search parameters
+        searchParameters: {},
+  
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+  
+        //... other Algolia params
+      },
     }),
 };
 
